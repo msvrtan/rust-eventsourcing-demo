626119799296246546b8f9f4b7999d473ddbeb41
Miro Svrtan <miro@mirosvrtan.me>
2019-03-31 21:17:03 +0200

    Use JsonFileBankAccountEventStore in examples

--- code/poc/ver5/src/examples.rs
+++ code/poc/ver5/src/examples.rs
@@ -5,7 +5,7 @@ use crate::deposit::DepositHandler;
 use crate::event::BankAccountEvent;
 use crate::event_store::BankAccountEventStore;
 use crate::open_bank_account::OpenBankAccountHandler;
-use crate::prelude::InMemoryBankAccountEventStore;
+use crate::prelude::JsonFileBankAccountEventStore;
 use crate::repository::BankAccountRepository;
 use crate::withdraw::WithdrawHandler;
 use std::sync::Arc;
@@ -20,7 +20,7 @@ pub fn examples() {
 }
 
 fn example_open_bank_account() {
-    let (repo, event_store) = build_repo(Vec::new());
+    let (repo, event_store) = build_repo(Vec::new(),"open".to_string());
     let handler = OpenBankAccountHandler::new(repo);
 
     let result = handler.handle(OpenBankAccount::new(100, 20));
@@ -36,7 +36,7 @@ fn example_open_bank_account() {
 fn example_deposit_money() {
     let initial_events = vec![BankAccountEvent::acc_opened(201, 20)];
 
-    let (repo, event_store) = build_repo(initial_events);
+    let (repo, event_store) = build_repo(initial_events,"deposit".to_string());
 
     let handler = DepositHandler::new(repo);
 
@@ -55,7 +55,7 @@ fn example_withdraw_money() {
         BankAccountEvent::acc_opened(302, 20),
         BankAccountEvent::credited(302, 49),
     ];
-    let (repo, event_store) = build_repo(initial_events);
+    let (repo, event_store) = build_repo(initial_events,"withdraw".to_string());
 
     let handler = WithdrawHandler::new(repo);
 
@@ -74,7 +74,7 @@ fn example_withdraw_refused() {
         BankAccountEvent::acc_opened(403, 20),
         BankAccountEvent::credited(403, 49),
     ];
-    let (repo, event_store) = build_repo(initial_events);
+    let (repo, event_store) = build_repo(initial_events,"withdraw_refused".to_string());
 
     let handler = WithdrawHandler::new(repo);
 
@@ -90,11 +90,15 @@ fn example_withdraw_refused() {
 
 type BuildRepoResult = (
     Arc<BankAccountRepository>,
-    Arc<InMemoryBankAccountEventStore>,
+    Arc<JsonFileBankAccountEventStore>,
 );
 
-fn build_repo(initial_events: Vec<BankAccountEvent>) -> BuildRepoResult {
-    let event_store = Arc::new(InMemoryBankAccountEventStore::new());
+fn build_repo(initial_events: Vec<BankAccountEvent>, file_name: String) -> BuildRepoResult {
+
+    let path ="/work/noob/rust-eventsourcing-demo/dev/code/poc/ver5/tmp/".to_string();
+    let file_path = format!("{}{}.json", path, file_name);
+
+    let event_store = Arc::new(JsonFileBankAccountEventStore::new(file_path));
     let event_store2 = event_store.clone();
     let repo = Arc::new(BankAccountRepository {
         event_store: event_store,
