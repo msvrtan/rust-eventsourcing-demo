57f0dff67c4a204a89002d25273dbdcf86d8eb2d
Miro Svrtan <miro@mirosvrtan.me>
2019-03-31 21:13:27 +0200

    TMP: disable running examples from versions 1-3

--- code/app/src/main.rs
+++ code/app/src/main.rs
@@ -5,9 +5,9 @@ extern crate ver4;
 extern crate ver5;
 
 fn main() {
-    ver1::examples::examples();
-    ver2::examples::examples();
-    ver3::examples::examples();
+    //ver1::examples::examples();
+    //ver2::examples::examples();
+    //ver3::examples::examples();
     ver4::examples::examples();
     ver5::examples::examples();
     println!("Hello, world!");
