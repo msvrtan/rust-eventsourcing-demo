52cc8ef8740b883116e3c472749cc391bcae95a2
Miro Svrtan <miro@mirosvrtan.me>
2019-03-31 21:05:19 +0200

    Add ver5 (copy of ver4)

--- code/app/src/main.rs
+++ code/app/src/main.rs
@@ -2,11 +2,13 @@ extern crate ver1;
 extern crate ver2;
 extern crate ver3;
 extern crate ver4;
+extern crate ver5;
 
 fn main() {
     ver1::examples::examples();
     ver2::examples::examples();
     ver3::examples::examples();
     ver4::examples::examples();
+    ver5::examples::examples();
     println!("Hello, world!");
 }
